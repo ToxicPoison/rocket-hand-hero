157250fe3503a6a890c7c6fa79c76009c8dba221
Jacob C <jacob.j.carlson03@gmail.com>
2024-02-01 17:54:02 -0800

    Fixed checkpoint sound being played when respawning

--- features/checkpoint/checkpoint.gd
+++ features/checkpoint/checkpoint.gd
@@ -7,6 +7,6 @@ func _ready():
 	area.body_entered.connect(on_body_entered)
 
 func on_body_entered(body):
-	if body is Player:
+	if body is Player and body.last_checkpoint != self:
 		body.last_checkpoint = self
 		$AudioStreamPlayer.play()
